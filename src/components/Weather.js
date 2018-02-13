499659851772146b3e119b63e4caf5b4b972f75d
Natalia Pilewska <pilewska.natalia@gmail.com>
2018-02-13 18:00:30 +0100

    Weather component added

---
+++ src/components/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../App.css'
+
+class Weather extends React.Component {
+    render () {
+        return(
+            <div>
+                Hello world 4
+            </div>
+
+        )
+    }
+}
+export default Weather
