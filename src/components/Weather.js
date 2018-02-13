3e7a573a19daaac016dc4f323673081e68ad75e4
Natalia Pilewska <pilewska.natalia@gmail.com>
2018-02-13 18:43:35 +0100

    fetching basic data from weather API

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,13 +2,41 @@ import React from 'react'
 import '../App.css'
 
 class Weather extends React.Component {
-    render () {
-        return(
+
+    state = {
+        weather: null
+
+    }
+
+    componentWillMount() {
+        this.getWeatherData()
+    }
+
+    getWeatherData = () => {
+        fetch("http://api.openweathermap.org/data/2.5/weather?q=Lublin,pl&units=metric&lang=pl&APPID=0b3d75e5a49f2a267f054a0a60bed6f3")
+            .then(response => response.json())
+            .then(dataWeather => this.setState({
+                weather: dataWeather
+
+            }))
+            .catch((err) => console.log(err))
+    }
+
+    render() {
+        console.log(this.state.weather)
+        return (
             <div>
-                Hello world 4
+                <div>
+                    {
+                        this.state.weather
+                        &&
+                        this.state.weather.name
+                    }
+                </div>
             </div>
 
         )
     }
 }
+
 export default Weather
